f688d49211ca3a5c8020c8b413e35b573d0b5e1b
SergioHMagalhaes <ssergioh3@gmail.com>
2024-04-09 14:06:56 -0300

    feat: layout tela home

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,8 +3,21 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {
+    path: 'home',
+    loadChildren: () => import('./pages/home/home.module').then(m => m.HomePageModule)
+  },
+  {
+    path: 'tab2',
+    loadChildren: () => import('./pages/tab2/tab2.module').then(m => m.Tab2PageModule)
+  },
+  {
+    path: 'tab3',
+    loadChildren: () => import('./pages/tab3/tab3.module').then(m => m.Tab3PageModule)
+  },
+  {
     path: '',
-    loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
+    redirectTo: '/home',
+    pathMatch: 'full'
   }
 ];
 @NgModule({
